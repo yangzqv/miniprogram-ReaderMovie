777ef80fb237fdf79f7be1142345792930a60c62
zq_prog <zq_prog@163.com>
2019-10-31 17:57:15 +0800

    电影页面数据绑定成功

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 App({
   globalData: {
     g_isPlayingMusic: false,      // 全局记录播放状态
-    g_isPlayingMusicPostId: null  // 确保是对应详情页文章的播放状态
+    g_isPlayingMusicPostId: null,  // 确保是对应详情页文章的播放状态
+    doubanBase: "http://t.yushu.im"
   }
 })
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,66 +1,61 @@
 // pages/movies/movies.js
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    movies: []
+  },
 
+  // 数据处理
+  processDoubanData: function(moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "..."
+      }
+      var temp = {
+        movieId: subject.id,
+        coverageUrl: subject.images.large,
+        title,
+        average: subject.rating.average,
+      }
+
+      movies.push(temp);
+      this.setData({
+        movies,
+      })
+    }
+  },
+
+  getMovieListData: function(url) {
+    var that = this;
+    wx.request({
+      url,
+      method: 'GET',
+      success: function(res) {
+        that.processDoubanData(res.data)
+      },
+      fail: function() {
+
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var inTheatersUrl = `${app.globalData.doubanBase}/v2/movie/in_theaters?start=0&count=3`;
+    var comingSoonUrl = `${app.globalData.doubanBase}/v2/movie/coming_soon?start=0&count=3`;
+    var top250Url = `${app.globalData.doubanBase}/v2/movie/top250?start=0&count=3`;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.getMovieListData(top250Url);
   }
+  
 })

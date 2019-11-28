05f2e8d439b84279b7b4c350ea9c7a852663b7fa
yangzq <yangzq_0214@163.com>
2019-11-28 14:28:33 +0800

    class 的简单使用

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,4 +1,4 @@
-var util = require('../../../utils/util.js');
+import { Movie } from './class/Movie.js';
 var app = getApp();
 
 Page({
@@ -9,44 +9,11 @@ Page({
   onLoad: function(options) {
     var movieId = options.id;
     var url = app.globalData.doubanBase + "/v2/movie/subject/" + movieId;
-    util.http(url, this.processDoubanData)
-  },
-
-  processDoubanData: function(data) {
-    if (!data) {
-      return;
-    }
-    var director = {
-      avatar: "",
-      name: "",
-      id: ""
-    }
-    if (data.directors[0] != null) {
-      if (data.directors[0].avatars != null) {
-        director.avatar = data.directors[0].avatars.large
-
-      }
-      director.name = data.directors[0].name;
-      director.id = data.directors[0].id;
-    }
-    var movie = {
-      movieImg: data.images ? data.images.large : "",
-      country: data.countries[0],
-      title: data.title,
-      originalTitle: data.original_title,
-      wishCount: data.wish_count,
-      commentCount: data.comments_count,
-      year: data.year,
-      generes: data.genres.join("、"),
-      stars: util.convertToStarsArray(data.rating.stars),
-      score: data.rating.average,
-      director: director,
-      casts: util.convertToCastString(data.casts),
-      castsInfo: util.convertToCastInfos(data.casts),
-      summary: data.summary
-    }
-    this.setData({
-      movie
+    var movie = new Movie(url);
+    movie.getMovieData(movie => {
+      this.setData({
+        movie
+      })
     })
   },
 
